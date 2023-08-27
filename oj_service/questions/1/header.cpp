e041365dd8cb1a7dc85aa2e7bee132f48310a83e
Jolly.Roger <104414865+Lp700750@users.noreply.github.com>
2023-08-27 21:34:52 +0800

    Add files via upload

---
+++ oj_service/questions/1/header.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution{
+    public:
+        bool isPalindrome(int x)
+        {
+            //将你的代码写在下面
+            
+            return true;
+        }
+};
